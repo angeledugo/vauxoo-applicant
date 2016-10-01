3eb78f7ca6be81af0215ae761b9495db6c98f4df
Angel Gonzalez <angeledugo@gmail.com>
2016-10-01 14:46:12 -0400

    se adicionan tabla hobby para adicionar dos hobby por empleado segun ejercicio sql2

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -16,6 +16,9 @@ CREATE TABLE employee_department (
 );
 
 CREATE TABLE employee_hobby (
+  id INT4 PRIMARY KEY,
+  name VARCHAR(80),
+  description VARCHAR(150)
 );
 
 
@@ -36,3 +39,37 @@ INSERT INTO employee(id, first_name, last_name,id_department) VALUES (1,'Angel',
 INSERT INTO employee(id, first_name, last_name,id_department) VALUES (2,'Pedro','Perez',1);
 INSERT INTO employee(id, first_name, last_name,id_department) VALUES (3,'Carlos','Gonzalez',2);
 INSERT INTO employee(id, first_name, last_name,id_department) VALUES (4,'Daniel','Rojas',4);
+
+
+-- Creacion de Tabla pivote para adicionar hobit a los empleados
+
+CREATE TABLE employee_hobby_pivot
+(
+  employee_id integer NOT NULL,
+  hobby_id integer NOT NULL,
+  CONSTRAINT employee_hobby_pivot_pkey PRIMARY KEY (employee_id,hobby_id),
+  CONSTRAINT employee_hobby_employee FOREIGN KEY (employee_id)
+  REFERENCES employee (id) MATCH SIMPLE
+  ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT employee_hobby_fkey FOREIGN KEY (hobby_id)
+  REFERENCES employee_hobby (id) MATCH SIMPLE
+  ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+
+-- Se Crean los 3 hobby
+INSERT into employee_hobby (id, name, description) VALUES (1,'Futbol','Deporte');
+INSERT into employee_hobby (id, name, description) VALUES (2,'Musica','Escuchar musica');
+INSERT into employee_hobby (id, name, description) VALUES (3,'Lectura','Leer libros diversos');
+
+-- se adicionan dos hobby por empleado
+INSERT into employee_hobby_pivot(employee_id, hobby_id) VALUES (1,1);
+INSERT into employee_hobby_pivot(employee_id, hobby_id) VALUES (1,2);
+
+INSERT into employee_hobby_pivot(employee_id, hobby_id) VALUES (2,1);
+INSERT into employee_hobby_pivot(employee_id, hobby_id) VALUES (2,3);
+
+INSERT into employee_hobby_pivot(employee_id, hobby_id) VALUES (3,2);
+INSERT into employee_hobby_pivot(employee_id, hobby_id) VALUES (3,1);
+
+INSERT into employee_hobby_pivot(employee_id, hobby_id) VALUES (4,3);
+INSERT into employee_hobby_pivot(employee_id, hobby_id) VALUES (4,1);

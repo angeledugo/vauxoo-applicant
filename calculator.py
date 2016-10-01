93105b592f90b79d3d78b1f1330cb2d9021300bf
Angel Gonzalez <angeledugo@gmail.com>
2016-10-01 14:49:45 -0400

    se crea la clase calculator para operacion de suma de una lista

--- calculator.py
+++ calculator.py
@@ -1,16 +1,19 @@
 """
-Your module documentation here
+Modulo creado para crear operaciones numericas
 """
 
 
 class CalculatorClass(object):
-    """
-    Your class documentation here
+    """Clase permite ejecutar metodos que simulan el proceso
+    de operaciones de  una calculadora
     """
 
     def sum(self, num_list):
+        """este metodo recibe una lista y nos permite sumar cada uno de
+        sus numeros
         """
-        Your method documentation here
-        """
-        # your sum code here
-        return "not implement yet"  # Remove this dummy line
+        suma = 0
+        # pylint: disable=C0103
+        for n in num_list:
+            suma += n
+            return suma
